c7110f70c344abad69466cdb5e14def069d97d19
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2019-11-26 16:48:51 -0300

    Form de Alta/edición persona

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatCardModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatIconModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule } from '@angular/material';
 import { ResumenEmpresaComponent } from './resumen-empresa/resumen-empresa.component';
 import { ListaPersonasComponent } from './lista-personas/lista-personas.component';
 import { DetallePersonaComponent } from './detalle-persona/detalle-persona.component';
 import { HttpClientModule }    from '@angular/common/http';
 import { ListaProyectosComponent } from './lista-proyectos/lista-proyectos.component';
 import { DetalleProyectoComponent } from './detalle-proyecto/detalle-proyecto.component';
+import { FormPersonaComponent } from './form-persona/form-persona.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,16 +21,22 @@ import { DetalleProyectoComponent } from './detalle-proyecto/detalle-proyecto.co
     ListaPersonasComponent,
     DetallePersonaComponent,
     ListaProyectosComponent,
-    DetalleProyectoComponent
+    DetalleProyectoComponent,
+    FormPersonaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule, 
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app/src/app/form-persona/form-persona.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Persona } from '../persona';
+import { PersonasService } from '../personas.service';
+
+@Component({
+  selector: 'app-form-persona',
+  templateUrl: './form-persona.component.html',
+  styleUrls: ['./form-persona.component.scss']
+})
+export class FormPersonaComponent implements OnInit {
+
+  editarPersona: boolean;
+  persona: Persona;
+
+  constructor(private personaService: PersonasService) { }
+
+  ngOnInit() {
+    this.personaService.editarPersona.subscribe(persona => {
+      this.persona = persona;
+      this.editarPersona = true;
+    });
+  }
+
+  onSave(): void {
+    this.personaService.guardar(this.persona).subscribe(() => this.onCancel());
+  }
+
+  onCancel(): void {
+    this.persona = new Persona();
+    this.persona.nueva = true;
+    this.editarPersona = false;
+  }
+}
