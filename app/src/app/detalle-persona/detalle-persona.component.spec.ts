f7e82e1a1d86ae17a6b406d15083a1641dc6b9e3
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2019-11-28 18:15:30 -0300

    Agrega caso de prueba de edición de persona

--- app/src/app/detalle-persona/detalle-persona.component.spec.ts
+++ app/src/app/detalle-persona/detalle-persona.component.spec.ts
@@ -6,21 +6,41 @@ import { Persona } from '../persona';
 import {
   MatCardModule, MatIconModule
 } from '@angular/material';
+import { PersonasService } from '../personas.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+class MockPersonasService extends PersonasService {
+  personaSeleccionada : Persona;
+  constructor(){
+    super(null);
+  }
+  personaSeleccionadaParaEdicion(persona : Persona) {
+    this.personaSeleccionada = persona;
+  }
+}
 
 describe('DetallePersonaComponent', () => {
   let component: DetallePersonaComponent;
   let fixture: ComponentFixture<DetallePersonaComponent>;
+  let personasService : MockPersonasService = new MockPersonasService();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         MatCardModule,
-        MatIconModule
+        MatIconModule,
+        HttpClientTestingModule
       ],
+      providers: [PersonasService],
       declarations: [ DetallePersonaComponent ]
     })
     .compileComponents();
+
+    TestBed.overrideComponent(
+      DetallePersonaComponent,
+      { set: { providers: [{ provide: PersonasService, useValue: personasService }] } }
+    );
+
 
     fixture = TestBed.createComponent(DetallePersonaComponent);
     component = fixture.componentInstance;
@@ -44,5 +64,11 @@ describe('DetallePersonaComponent', () => {
   it(`should show the person name`, () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.mat-card-title').textContent).toContain('Santiago');
+  });
+
+  it(`should emit edit event`, () => {
+    const compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('.mat-icon').click();
+    expect(personasService.personaSeleccionada).toBe(component.persona);
   });
 });
